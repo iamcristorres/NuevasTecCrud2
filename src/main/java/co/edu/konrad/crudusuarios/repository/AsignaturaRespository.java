9138312f927f5b05f9161b25aa8ee5f34c0b3d36
Andres <andres.franco.lopez0@gmail.com>
2020-10-21 21:00:59 -0500

    Dependencias añadidas
    Se añaden las dependencias al archivo AsignaturaRespository.java

--- src/main/java/co/edu/konrad/crudusuarios/repository/AsignaturaRespository.java
+++ src/main/java/co/edu/konrad/crudusuarios/repository/AsignaturaRespository.java
@@ -1,6 +1,8 @@
 package co.edu.konrad.crudusuarios.repository;
 
-
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import co.edu.konrad.crudusuarios.model.Asignatura;
 
 @Repository
 public interface AsignaturaRespository extends CrudRepository<Asignatura,Long>{
